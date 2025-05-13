8a829e02c472f8268a04f143d5b410609b3d9c23
Nizamudeen N <nizamudeen@solwyz.com>
2025-05-13 15:16:25 +0530

    footer updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,16 @@ export default {
   ],
   theme: {
     extend: {
-      fontFamily:{
-        manrope:['Manrope', 'sans-serif'],
-        
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
       },
-      backgroundColor:{
-        "bgColor":'#010B02'
-      }
+      backgroundColor: {
+        bgColor: '#010B02',
+      },
+      backgroundImage: {
+        'footerMap': "url('/src/assets/footer/world 1.png')", // Change the path accordingly
+        'footerMapMob': "url('/src/assets/footer/footer mobile.png')", // Change the path accordingly
+      },
     },
   },
   plugins: [],
