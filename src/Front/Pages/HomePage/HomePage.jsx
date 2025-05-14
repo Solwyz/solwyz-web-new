87c1b904b92cdf5e34746645d4ba8238657fa2c5
Ruthe46 <ruthinlosh.web.solwyz@gmail.com>
2025-05-14 17:40:36 +0530

    Latest buzz

--- src/Front/Pages/HomePage/HomePage.jsx
+++ src/Front/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import HomeBanner from '../../Components/HomeBanner/HomeBanner'
+import WebDesign from '../../Components/HomeComponents/WebDesign/WebDesign'
+import RequestAudit from '../../Components/HomeComponents/RequestAudit/RequestAudit'
+import LatestBuzz from '../../Components/HomeComponents/LatestBuzz/LatestBuzz'
 
 function HomePage() {
   return (
     <div className=''>
       <HomeBanner/>
+      <WebDesign/>
+      <RequestAudit/>
+      <LatestBuzz/>
     </div>
   )
 }
