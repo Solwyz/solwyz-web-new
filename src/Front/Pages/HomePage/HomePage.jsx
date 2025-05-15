f52460519e448e7d62328207236fce263e5cd41b
Nizamudeen N <nizamudeen@solwyz.com>
2025-05-15 11:37:00 +0530

    contactPage

--- src/Front/Pages/HomePage/HomePage.jsx
+++ src/Front/Pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HomeBanner from '../../Components/HomeBanner/HomeBanner'
 import OurOffice from '../../Components/HomeComponents/OurOffice/OurOffice'
 import Empowering from '../../Components/HomeComponents/EmpoweringHome/Empowering'
+import HomeContact from '../../Components/HomeComponents/HomeContact/HomeContact'
 
 function HomePage() {
   return (
     <div className=''>
       <HomeBanner/>
       <OurOffice/>
+      <HomeContact/>
       <Empowering/>
     
     </div>
