4bf1a3f3daa684241771620f437f0a82ff40d70c
Nizamudeen N <nizamudeen@solwyz.com>
2025-05-15 13:18:57 +0530

    who we are

--- src/Front/Pages/HomePage/HomePage.jsx
+++ src/Front/Pages/HomePage/HomePage.jsx
@@ -8,12 +8,14 @@ import LatestBuzz from '../../Components/HomeComponents/LatestBuzz/LatestBuzz'
 import OurOffice from '../../Components/HomeComponents/OurOffice/OurOffice'
 import Empowering from '../../Components/HomeComponents/EmpoweringHome/Empowering'
 import HomeContact from '../../Components/HomeComponents/HomeContact/HomeContact'
+import WhoweAre from '../../Components/HomeComponents/WhoweAre/WhoweAre'
 
 
 function HomePage() {
   return (
     <div className=''>
       <HomeBanner/>
+      <WhoweAre/>
 
       <WebDesign/>
       <RequestAudit/>
